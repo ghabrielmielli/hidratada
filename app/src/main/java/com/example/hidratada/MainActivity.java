5bd87a021ce263824893937edb58e4b86e81d100
Ghabriel Mielli <ghabrielmielli@hotmail.com>
2021-06-14 15:04:54 -0400

    Demais informações para o adapterView implementadas; Funcionamento mock do botão de inserção.

--- app/src/main/java/com/example/hidratada/MainActivity.java
+++ app/src/main/java/com/example/hidratada/MainActivity.java
@@ -5,8 +5,10 @@ import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
 
+import com.example.hidratada.model.Intervalo;
 import com.example.hidratada.model.Planta;
 import com.example.hidratada.model.PlantaList;
 
@@ -14,7 +16,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-
+    private PlantaAdapter adapterPlanta;
+    private RecyclerView rv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,11 +25,17 @@ public class MainActivity extends AppCompatActivity {
 
         List<Planta> plantas = PlantaList.getFakePlantas();
 
-        PlantaAdapter adapterPlanta = new PlantaAdapter(plantas);
+        adapterPlanta = new PlantaAdapter(plantas);
 
-        RecyclerView rv = findViewById(R.id.recycler_view_main);
+        rv = findViewById(R.id.recycler_view_main);
         rv.setAdapter(adapterPlanta);
 
 
+    }
+
+    public void addPlanta(View view){
+    adapterPlanta.getPlantas().add(0, new Planta("laurinha", new Intervalo(true, 2), 3));
+    adapterPlanta.notifyItemInserted(0);
+    rv.scrollToPosition(0);
     }
 }
--- app/src/main/java/com/example/hidratada/PlantaAdapter.java
+++ app/src/main/java/com/example/hidratada/PlantaAdapter.java
@@ -1,12 +1,17 @@
 package com.example.hidratada;
 
+import android.graphics.Color;
+import android.os.Build;
 import android.text.Layout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
+import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.hidratada.model.Planta;
@@ -30,6 +35,7 @@ public class PlantaAdapter extends RecyclerView.Adapter<PlantaAdapter.PlantaView
         return new PlantaViewHolder(view);
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     public void onBindViewHolder(@NonNull PlantaViewHolder holder, int position) {
         Planta planta = plantas.get(position);
@@ -41,16 +47,41 @@ public class PlantaAdapter extends RecyclerView.Adapter<PlantaAdapter.PlantaView
         return plantas.size();
     }
 
+    public  List<Planta> getPlantas() {
+        return plantas;
+    }
+
     class PlantaViewHolder extends RecyclerView.ViewHolder {
 
-        TextView txtNome;
+        TextView txtNome, txtIcone;
+        ImageView imgNvAgua1, imgNvAgua2, imgNvAgua3, imgNvAgua4;
+
         public PlantaViewHolder(@NonNull View itemView) {
             super(itemView);
             txtNome = itemView.findViewById(R.id.txt_nome_planta);
+            txtIcone = itemView.findViewById(R.id.txt_icone);
+            imgNvAgua1 = itemView.findViewById(R.id.img_nivel_agua_1);
+            imgNvAgua2 = itemView.findViewById(R.id.img_nivel_agua_2);
+            imgNvAgua3 = itemView.findViewById(R.id.img_nivel_agua_3);
+            imgNvAgua4 = itemView.findViewById(R.id.img_nivel_agua_4);
         }
 
+        @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
         public void bind(Planta planta){
             txtNome.setText(planta.getNome());
+            txtIcone.setText(Character.toString(planta.getNome().charAt(0)).toUpperCase());
+            switch (planta.getQuantidadeDeAgua()) {
+                case 4:
+                    imgNvAgua4.setColorFilter(ContextCompat.getColor(imgNvAgua4.getContext(), R.color.floresta_umida_variante), android.graphics.PorterDuff.Mode.SRC_IN);
+                case 3:
+                    imgNvAgua3.setColorFilter(ContextCompat.getColor(imgNvAgua3.getContext(), R.color.floresta_umida_variante), android.graphics.PorterDuff.Mode.SRC_IN);
+                case 2:
+                    imgNvAgua2.setColorFilter(ContextCompat.getColor(imgNvAgua2.getContext(), R.color.floresta_umida_variante), android.graphics.PorterDuff.Mode.SRC_IN);
+                case 1:
+                    imgNvAgua1.setColorFilter(ContextCompat.getColor(imgNvAgua1.getContext(), R.color.floresta_umida_variante), android.graphics.PorterDuff.Mode.SRC_IN);
+
+                default:
+            }
         }
     }
 
